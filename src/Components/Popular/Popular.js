f32a8c0d6e1e987549522263c1396213316eda96
MuhammadHamza9025 <mhamza9025@gmail.com>
2024-07-22 10:46:20 +0500

    k

--- src/Components/Popular/Popular.js
+++ src/Components/Popular/Popular.js
@@ -9,7 +9,7 @@ const Popular = () => {
                 {data.map((items) => {
                     return (
                         <>
-                            <div className='w-[150px] sm:w-[19vw]  m-[5px] sm:m-[7px] lg:m-[12px]' key={items.id}>
+                            <div className='w-[152px] sm:w-[19vw]  m-[5px] sm:m-[7px] lg:m-[12px]' key={items.id}>
                                 <img src={items.image} alt="" className='h-[140px] sm:h-[200px] lg:h-[250px] w-[100%]' />
                                 <span className='text-gray-800 max-w-[100%] my-10 text-xs sm:text-sm lg:text-balance'>{items.name}</span>
                                 <br />
