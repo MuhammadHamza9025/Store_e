452d284725c93c2de113b22dcdfa2448056c8831
MuhammadHamza9025 <mhamza9025@gmail.com>
2024-07-23 16:36:05 +0500

    lk

--- src/Components/Items/Items.js
+++ src/Components/Items/Items.js
@@ -17,8 +17,8 @@ const Items = (props) => {
                     <img src={props.image} alt="" className='h-[140px] sm:h-[200px] lg:h-[250px] w-[100%]' />
                     <span className='text-gray-800 max-w-[100%] my-10 text-xs sm:text-sm lg:text-balance'>{props.name}</span>
                     <br />
-                    <span className='text-sx'>${props.new_price}</span>
-                    <span className='line-through mx-10 text-gray-400 text-xs'>${props.old_price}</span>
+                    <span className='text-xs text-red-500 shadow-md'>${props.new_price}</span>
+                    <span className='line-through mx-10 italic text-gray-400 text-xs '>${props.old_price}</span>
                     {/* <div className='absolute z-9 -top-2 blinking-image'><img src={newicon} alt="" className='h-[50px] -rotate-45 ' /></div> */}
                 </div>
             </Link>
